a124139fa0c074ba189255a1c96758dfb686c61f
melvan27 <119530410+melvan27@users.noreply.github.com>
2024-09-15 21:08:30 -0500

    add widget_tweaks and login redirect URL

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
